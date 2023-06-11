ae1ef28470f594132856faa6f045fafb0f1e69a3
vishalgoswami12 <vishalgoswami2250@gmail.com>
2023-06-11 18:04:43 +0530

    vue-todo

---
+++ todo/src/components/Form.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+      <h2>Todo List</h2>
+      <form @submit.prevent="addTodo">
+        <input type="text" v-model="newTodo" placeholder="Enter a new todo" />
+        <button type="submit">Add Todo</button>
+      </form>
+  
+      <ul>
+        <li v-for="(todo, index) in todos" :key="index">
+          {{ todo }}
+          <button @click="removeTodo(index)">Remove</button>
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  import { ref } from 'vue';
+  
+  export default {
+    name: 'TodoList',
+    setup() {
+      const todos = ref([]);
+      const newTodo = ref('');
+  
+      const addTodo = () => {
+        if (newTodo.value.trim() !== '') {
+          todos.value.push(newTodo.value);
+          newTodo.value = '';
+        }
+      };
+  
+      const removeTodo = (index) => {
+        todos.value.splice(index, 1);
+      };
+  
+      return {
+        todos,
+        newTodo,
+        addTodo,
+        removeTodo,
+      };
+    },
+  };
+  </script>
---
+++ todo/src/components/Todo.vue
@@ -0,0 +1,20 @@
+<template>
+
+    <h2>{{ ele.item }}</h2>
+    <button @click="handleDelete(ele.id)">Delete</button>
+</template>
+
+<script setup>
+
+import { defineProps } from 'vue';
+defineProps({
+    ele:{
+        type:Object,
+        required:true,
+    },
+    handleDelete:{
+        type:Function,
+        required:true,
+    }
+})
+</script>
